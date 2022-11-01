da9b00bb0d9565a31eca9c4200208848c5449bee
DESKTOP-06OEET5\ldw <smartin1028@outlook.kr>
2022-11-01 11:27:25 +0900

    init

---
+++ src/main/java/mvn_test/MvnTestApplication.java
@@ -0,0 +1,13 @@
+package mvn_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvnTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvnTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mvn_test/MvnTestApplicationTests.java
@@ -0,0 +1,13 @@
+package mvn_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvnTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
